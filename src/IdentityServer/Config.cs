efdcff5b064b41cd60470814e0fbaabee20f3350
wbasu15 <wamika.basu@carsales.com.au>
2022-04-04 18:27:30 +0530

    Protecting an API using Client Credentials

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -5,16 +5,35 @@ namespace IdentityServer;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
+        new List<IdentityResource>
+        {
             new IdentityResources.OpenId()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            { };
+        new List<ApiScope>
+        { 
+            new ApiScope("api1", "My API") 
+        };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
 }
